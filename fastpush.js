782e0d229e2bb77af881ca7d5aa74b3890facb2e
Jón Prüßmeier <jon.pruessmeier@clockin.de>
2023-05-31 14:42:40 +0200

    HEllo

---
+++ fastpush.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+const { exec } = require('child_process');
+
+function executeCommand(command) {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+      resolve(stdout.trim());
+    });
+  });
+}
+
+async function fastpush() {
+  try {
+    const commitMessage = process.argv[2];
+    if (!commitMessage) {
+      console.error('Please provide a commit message.');
+      return;
+    }
+
+    await executeCommand('git add .');
+    await executeCommand(`git commit -m "${commitMessage}"`);
+    await executeCommand('git push');
+
+    console.log('Pushed to remote repository successfully.');
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+}
+
+fastpush();
