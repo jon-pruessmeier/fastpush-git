4d95a7e088b4b8170aa99790f1e044b7d405455e
Jón Prüßmeier <jon.pruessmeier@clockin.de>
2023-05-31 14:56:04 +0200

    prettier-config

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 #!/usr/bin/env node
 
-const { exec } = require("child_process");
+const { exec } = require('child_process')
 
 function executeCommand(command) {
   return new Promise((resolve, reject) => {
     exec(command, (error, stdout, stderr) => {
       if (error) {
-        reject(error);
-        return;
+        reject(error)
+        return
       }
-      resolve(stdout.trim());
-    });
-  });
+      resolve(stdout.trim())
+    })
+  })
 }
 
 async function fastpush() {
   try {
-    const commitMessage = process.argv[2];
+    const commitMessage = process.argv[2]
     if (!commitMessage) {
-      console.error("Please provide a commit message.");
-      return;
+      console.error('Please provide a commit message.')
+      return
     }
 
-    await executeCommand("git add .");
-    await executeCommand(`git commit -m "${commitMessage}"`);
-    await executeCommand("git push");
+    await executeCommand('git add .')
+    await executeCommand(`git commit -m "${commitMessage}"`)
+    await executeCommand('git push')
 
-    console.log("Pushed to remote repository successfully.");
+    console.log('Pushed to remote repository successfully.')
   } catch (error) {
-    console.error("An error occurred:", error);
+    console.error('An error occurred:', error)
   }
 }
 
 //initialise command
-fastpush();
+fastpush()
